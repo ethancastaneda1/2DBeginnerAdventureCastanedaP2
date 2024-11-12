ccfc9ec0084ba38f72d4fb9e65d94f680a1052a7
ethancastaneda1 <726750@student.dusd.net>
2024-11-12 10:04:18 -0800

    FInished up animations
    edited Scripts and finished animations on both Vending Machine and Sugar Land

--- 2DBeginnerAdventureP2Castaneda/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureP2Castaneda/Assets/Scripts/EnemyController.cs
@@ -11,11 +11,13 @@ public class EnemyController : MonoBehaviour
 
     float timer;
     int direction = 1;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changetime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -33,11 +35,15 @@ public class EnemyController : MonoBehaviour
         Vector2 position = rigidbody2d.position;
         if (vertical)
         {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
             position.y = position.y + Time.deltaTime * speed * direction;
 
         }
         else
         {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
             position.x = position.x + Time.deltaTime * speed * direction;
         }
         
--- 2DBeginnerAdventureP2Castaneda/Assets/Scripts/SugarLandController.cs
+++ 2DBeginnerAdventureP2Castaneda/Assets/Scripts/SugarLandController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SugarLandController : MonoBehaviour
@@ -9,7 +10,7 @@ public class SugarLandController : MonoBehaviour
     public int maxHealth = 5;
     int currentHealth;
 
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.0f;
 
     bool isInvincible;
     float invincibleTimer;
@@ -19,12 +20,16 @@ public class SugarLandController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         // currentHealth = maxHealth;
         currentHealth = 5;
+        animator = GetComponent<Animator>();
            
     }
 
@@ -34,6 +39,15 @@ public class SugarLandController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
@@ -58,6 +72,7 @@ public class SugarLandController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
             {
                 return;
